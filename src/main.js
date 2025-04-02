013567c8aae9cf5aef8dc482b509d6416dae63e6
Lynn Martin <167239437+elvishcraftsman@users.noreply.github.com>
2025-04-02 21:49:17 +0000

    Update main.js to 2.1.3

--- src/main.js
+++ src/main.js
@@ -36,7 +36,7 @@ export const TimeTrackerApplication = GObject.registerClass(
                     application_name: 'time-tracker',
                     application_icon: 'com.lynnmichaelmartin.TimeTracker',
                     developer_name: 'Lynn Martin',
-                    version: '2.1.2',
+                    version: '2.1.3',
                     developers: [
                         'Lynn Martin'
                     ],
